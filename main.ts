5a9cebe6956f57eb10e94f4c9ec0ff52110e5e82
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-12 17:48:34 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-let cislo = 0
-let opakuj = 0
-basic.forever(function () {
+function pokyn () {
     while (opakuj == 0) {
         basic.clearScreen()
         basic.showNumber(cislo)
         basic.pause(1000)
         cislo += 1
     }
+}
+input.onButtonPressed(Button.A, function () {
+    pokyn()
 })
+input.onButtonPressed(Button.B, function () {
+    control.reset()
+})
+let opakuj = 0
+let cislo = 0
+cislo = 0
+opakuj = 0
